ca3e82a48ad2e1300d5f937e9826b922c93dbcb5
Kyle Dooley <kdooley0727@gmail.com>
2024-02-21 16:11:18 -0800

    Add files via upload

--- main.js
+++ main.js
@@ -4,6 +4,12 @@ let search = document.querySelector('.search-box');
 // Select the search icon element
 let searchIcon = document.querySelector('#search-icon');
 
+// Select the response container
+// Select the response container
+let responseContainer = document.querySelector('#response-container');
+
+let brandSearchInput = document.querySelector('#brand-search');
+
 // Add a click event listener to the search icon
 searchIcon.addEventListener('click', () => {
     // Toggle the 'active' class on the search box
@@ -14,16 +20,16 @@ searchIcon.addEventListener('click', () => {
         const options = {
             method: 'GET',
             headers: {
-              'X-RapidAPI-Key': 'f93b096e20msh06729fb32d55626p128c2bjsn645b069521a7',
-              'X-RapidAPI-Host': 'cis-automotive.p.rapidapi.com'
+                'X-RapidAPI-Key': 'f93b096e20msh06729fb32d55626p128c2bjsn645b069521a7',
+                'X-RapidAPI-Host': 'cis-automotive.p.rapidapi.com'
             }
         };
 
         fetch(url, options)
             .then(response => response.json())
             .then(data => {
-                // Process the data returned by the API
-                console.log(data);
+                // Update the response container with the API response
+                responseContainer.innerText = JSON.stringify(data);
             })
             .catch(error => {
                 console.error('Error fetching data:', error);
