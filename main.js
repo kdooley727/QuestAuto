e0ba8c115b3141be6e762fe0e22d2d2bc2f90681
Kyle Dooley <kdooley0727@gmail.com>
2024-03-02 11:12:51 -0800

    updating address for api in main.js

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-app.use((req, res, next) => {
+/* app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
     next();
 });
@@ -20,3 +20,32 @@ function handleSearch() {
 
 // Add event listener to search icon
 document.getElementById('search-button').addEventListener('click', handleSearch);
+ */
+
+fetch('http://questapi.zapto.org:8080/${makeName}')
+  .then(response => response.json())
+  .then(data => {
+    // Use the fetched data in your website
+    console.log(data);
+  })
+  .catch(error => {
+    console.error('Error fetching data:', error);
+  });
+
+  fetch('http://questapi.zapto.org:8080/${makeName}', {
+  method: 'PUT',
+  headers: {
+    'Content-Type': 'application/json'
+  },
+  body: JSON.stringify({ key: 'value' })
+})
+.then(response => {
+  if (response.ok) {
+    console.log('Resource updated successfully');
+  } else {
+    console.error('Failed to update resource:', response.status);
+  }
+})
+.catch(error => {
+  console.error('Error updating resource:', error);
+});
