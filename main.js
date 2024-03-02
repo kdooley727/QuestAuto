2e8a2e7816c859d0eabf2bea5fa3db35375c9ea1
Kyle Dooley <kdooley0727@gmail.com>
2024-03-02 12:44:39 -0800

    updating address for api in main.js

--- main.js
+++ main.js
@@ -1,27 +1,3 @@
-app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    next();
-});
-/* 
-// Function to handle search based on input value
-function handleSearch() {
-    const makeName = document.getElementById('make-search').value;
-    
-    fetch(`http://questapi.zapto.org:8080/${makeName}`)
-
-        .then(response => response.json())
-        .then(data => {
-            console.log('Cars:', data);
-            // Update the page with the search results
-            // (You can implement this part based on your website's layout)
-        })
-        .catch(error => console.error('Error:', error));
-}
-
-// Add event listener to search icon
-document.getElementById('search-button').addEventListener('click', handleSearch);
- */
-
 fetch(`http://questapi.zapto.org:8080/${makeName}`)
   .then(response => response.json())
   .then(data => {
