3357a8970da043b8cb1c935a0cfcc5a25eed04ca
Kyle Dooley <kdooley0727@gmail.com>
2024-03-02 10:17:07 -0800

    updating address for api in main.js

--- main.js
+++ main.js
@@ -2,7 +2,7 @@
 function handleSearch() {
     const makeName = document.getElementById('make-search').value;
     
-    fetch(`https://questapi.zapto.org/${makeName}`)
+    fetch(`http://34.125.79.155${makeName}`)
         .then(response => response.json())
         .then(data => {
             console.log('Cars:', data);
